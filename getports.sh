a1dd7e189b003e9a98ee07258a81b6df207bca34
Alfonso Farías <77316261+chronologie7@users.noreply.github.com>
2022-08-13 22:07:08 -0400

    initial file

---
+++ getports.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z $1 ]
+then
+  echo "Error, no argumments!";
+  exit 1;
+elif ! [ -f $1 ]
+then
+  echo "Error, no regular file!";
+  exit 1;
+fi
+dataports=$(cat $1 | grep -oP '[0-9]{1,5}/open' | awk -F / '{print $1}' | xargs | tr ' ' ',')
+ipdata=$(cat $1 | awk FNR==3 | cut -d " " -f2)
+echo "(*) Proccessig data..."
+echo -e "\t(*) IP Address\t: $ipdata"
+echo -e "\t(*) Ports\t: $dataports"
+echo $dataports | xclip -sel clip
+echo "(*) Ports copied to clipboard!"
